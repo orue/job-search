ef07c34f2c1b5432105c9f5957bc68192fe95f26
Carlos Orue <carlos@orue.io>
2023-10-08 15:17:46 -0500

    setup and style MainNav

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,4 @@
+/*eslint-env node*/
 /** @type {import('tailwindcss').Config} */
 const defaultTheme = require('tailwindcss/defaultTheme')
 
@@ -7,6 +8,11 @@ module.exports = {
     extend: {
       fontFamily: {
         sans: ['Open Sans', ...defaultTheme.fontFamily.sans]
+      },
+      colors: {
+        'brand-gray-1': '#dadce0',
+        'brand-blue-1': '#1967d2',
+        'brand-green-1': '137333'
       }
     }
   },
